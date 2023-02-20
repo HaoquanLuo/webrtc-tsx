b75420d90aebc5e7885300ee62b5ad7c924c55ff
luohao <1223048059@qq.com>
2023-02-21 01:31:06 +0800

    chore: add vitest

---
+++ yike-client/tests/index.spec.ts
@@ -0,0 +1,10 @@
+import { expect, test } from 'vitest'
+import LoadingBox from '../src/components/LoadingBox'
+
+test('first test', () => {
+  expect(1 + 1).toBe(2)
+})
+
+test('hello', () => {
+  console.log(LoadingBox.toString())
+})
