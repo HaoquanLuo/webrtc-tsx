32b42a9c8731b88725feea545ae2b4dbed3554fd
Haoquan Luo <94098205+ffxixslh@users.noreply.github.com>
2023-05-07 15:48:18 +0800

    chore: Update mailerConfing.ts

--- yike-server/src/server/mailer/mailerConfing.ts
+++ yike-server/src/server/mailer/mailerConfing.ts
@@ -2,8 +2,8 @@ export default {
   service: 'qq', //使用了内置传输发送邮件 查看支持列表：https://nodemailer.com/smtp/well-known/
   port: 456, // SMTP 端口
   auth: {
-    user: '1223048059@qq.com', // 邮箱
-    pass: 'esianjqbsvatbagh', // 这里密码不是邮箱密码，是你设置的smtp授权码
+    user: '', // 邮箱
+    pass: '', // 这里密码不是邮箱密码，是你设置的smtp授权码
   },
   secureConnection: true, // 使用 SSL
 }
